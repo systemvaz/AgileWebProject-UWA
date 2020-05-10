1965b46d3e9e0684498de4f4c453e902ff116dbd
systemvaz <systemvaz@outlook.com>
2020-05-10 22:57:08 +0800

    base setup with db

--- config.py
+++ config.py
@@ -1,4 +1,9 @@
 import os
 
+basedir = os.path.abspath(os.path.dirname(__file__))
+
 class Config(object):
     SECRET_KEY = os.environ.get('AWD_PROJECT_KEY') or 'dlaskjaskld3290432LKNlk4094'
+    # Database config
+    SQLALCHEMY_DATABASE_URI = os.environ.get('AWD_DATABASE_URL') or 'sqlite:///' + os.path.join(basedir, 'database/app.db')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False

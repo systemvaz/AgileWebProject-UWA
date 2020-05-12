9a7b20b329001f2d86e96516ebf57a605b4eede1
systemvaz <systemvaz@outlook.com>
2020-05-12 17:22:00 +0800

    proposed schema

--- app/models.py
+++ app/models.py
@@ -3,7 +3,7 @@ from datetime import datetime
 from flask_login import UserMixin
 from werkzeug.security import generate_password_hash, check_password_hash
 
-# Define our dimension tables
+# DB dimension tables
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
@@ -15,13 +15,10 @@ class User(UserMixin, db.Model):
 
     def set_password(self, password):
         self.password_hash = generate_password_hash(password)
-
     def check_password(self, password):
         return check_password_hash(self.password_hash, password)
-
     def check_admin(self):
         return self.admin
-
     def __repr__(self):
         return '<User: {}>'.format(self.username)
 
@@ -42,18 +39,20 @@ class Multichoice(db.Model):
     answer_selection = db.Column(db.String(128))
     is_correct = db.Column(db.Boolean)  
 
-class Attempted(db.Model):
+class Attempts(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
     qset_id = db.Column(db.Integer, db.ForeignKey('qset.id'))
     timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
 
-class Answer(db.Model):
+# DB Fact table
+class Results(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
     question_id = db.Column(db.Integer, db.ForeignKey('question.id'))
     answer_txt = db.Column(db.String(256))
     answer_mc = db.Column(db.Integer, db.ForeignKey('multichoice.id'))
+    is_needs_review = db.Column(db.Boolean)
     is_correct = db.Column(db.Boolean)
 
 

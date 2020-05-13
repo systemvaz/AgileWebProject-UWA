cf549a4e01ab8f902f8b7a66f422c343f62619e6
systemvaz <systemvaz@outlook.com>
2020-05-13 16:32:58 +0800

    updated user fields in db

--- app/models.py
+++ app/models.py
@@ -1,4 +1,5 @@
 from app import db, login
+from hashlib import md5
 from datetime import datetime
 from flask_login import UserMixin
 from werkzeug.security import generate_password_hash, check_password_hash
@@ -10,6 +11,8 @@ class User(UserMixin, db.Model):
     first_name = db.Column(db.String(64))
     last_name = db.Column(db.String(64))
     email = db.Column(db.String(120), index=True, unique=True)
+    about_me = db.Column(db.String(140))
+    last_seen = db.Column(db.DateTime, default=datetime.utcnow)
     password_hash = db.Column(db.String(128))
     is_admin = db.Column(db.Boolean, index=True)
 
@@ -19,6 +22,9 @@ class User(UserMixin, db.Model):
         return check_password_hash(self.password_hash, password)
     def check_admin(self):
         return self.admin
+    def avatar(self, size):
+        digest = md5(self.email.lower().encode('utf-8')).hexdigest()
+        return 'https://www.gravatar.com/avatar/{}?d=identicon&s={}'.format(digest, size)
     def __repr__(self):
         return '<User: {}>'.format(self.username)
 

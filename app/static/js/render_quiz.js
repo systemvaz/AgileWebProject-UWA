647b5f22209c308e99d8217a88fa0cf6a0b08876
systemvaz <systemvaz@outlook.com>
2020-05-16 14:58:23 +0800

    refactoring

---
+++ app/static/js/render_quiz.js
@@ -0,0 +1,9 @@
+// --------------------------------------------------------------------------
+// Define Javascript functions allowing for dynamic rendering of quizes
+// and form controls for users in the /take_quiz route.
+// --------------------------------------------------------------------------
+
+function render_quiz(questions, multichoice)
+{
+    
+}
